a02e44d25d3eb92087a5c8086a15f5797920f5b0
Daniel Machado <daniel7oficial@gmail.com>
2023-08-01 15:19:27 -0300

    push notifcations

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import { StatusBar, View } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
+import OneSignal from 'react-native-onesignal';
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
 
 import { Routes } from '@routes/index';
 
 import { THEME } from './src/theme';
 import { Loading } from '@components/loading';
+
+OneSignal.setAppId('2593f5de-0ca2-40a9-839a-95ae97f7360a');
+
+OneSignal.promptForPushNotificationsWithUserResponse(response => {
+  console.log(response);
+})
 
 export default function App() {
   const  [fontLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
